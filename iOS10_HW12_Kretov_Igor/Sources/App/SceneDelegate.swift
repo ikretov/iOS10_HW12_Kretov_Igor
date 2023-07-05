afc1b4e644d431085f6a1f5e5b9d3df2566df2f1
i_kretov <kretov.ig@mail.ru>
2023-07-06 00:24:23 +0900

    start_settings
    создание папок и убрал лишние методы

---
+++ iOS10_HW12_Kretov_Igor/Sources/App/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  iOS10_HW12_Kretov_Igor
+//
+//  Created by Igor Kretov on 06.07.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
