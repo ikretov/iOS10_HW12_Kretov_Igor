7aa62104a60c4bd6873371c1b14313ccf2c57071
i_kretov <kretov.ig@mail.ru>
2023-07-07 22:58:08 +0900

    add ViewController

---
+++ iOS10_HW12_Kretov_Igor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS10_HW12_Kretov_Igor
+//
+//  Created by Igor Kretov on 06.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
